a087a23a727377856e23e9f887f022364a463bb0
Armaan Dhaliwal-McLeod <armcleod@microsoft.com>
2019-07-25 12:13:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Azure Virtual Machine Powershell Script
+
+Automates Azure virtual machine creation with Powershell. Make sure you have a subscription set up before following the below steps. 
+
+## Installation
+
+1. Make sure you have Git installed. 
+2. Clone repository with `git clone`
+3. [Install Azure PowerShell module](https://docs.microsoft.com/en-us/powershell/azure/install-az-ps?view=azps-2.4.0)
+
+## Configuration
+
+Create a `config.json` file in the same folder as the `vmcreate.ps1` script, and follow the format below:
+
+```
+{
+    "resourceGroup": "beijing",
+    "location": "East Asia",
+    "virtualNetworkName": "myVirtualNetwork",
+    "subnetName": "mySubnet",
+    "networkSecurityGroupName": "myNetworkSecurityGroup",
+    "networkSecurityGroupRuleRDPName": "myNetworkSecurityGroupRuleRDP",
+    "vms": [
+        {
+            "name": "server1",
+            "size": "Standard_D1",
+            "publisherName": "MicrosoftWindowsServer",
+            "offer": "WindowsServer",
+            "skus": "2016-Datacenter"
+        },
+        {
+            "name": "server2",
+            "size": "Standard_D1",
+            "publisherName": "MicrosoftWindowsServer",
+            "offer": "WindowsServer",
+            "skus": "2019-Datacenter"
+        }
+    ]
+}
+```
+
+The above is an example of creating two Windows Servers with resource group *beijing* located in *East Asia*. 
+
+You can have a look at the [Windows Virtual Machines Documentation](https://docs.microsoft.com/en-us/azure/virtual-machines/windows/) to customize your virtual machines to your liking. 
+
+## Running Script
+
+    PS C:\PATH> .\vmcreate.ps1 
+
+Where **PATH** is your local path to the cloned repo. 
+
+Observe your azure portal and wait for your virtual machines to get created. 
+
+
+
+
---
+++ vmcreate.ps1
@@ -0,0 +1,102 @@
+# Suppress script warnings
+Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
+
+# Login to Azure account
+Write-Output "Pulling Azure account credentials..."
+Login-AzAccount
+
+# Extract JSON config data
+$config = Get-Content .\config.json | ConvertFrom-Json
+
+# Check if resource group has already been added
+Get-AzResourceGroup `
+    -Name $config.resourceGroup `
+    -ErrorVariable notPresent `
+    -ErrorAction SilentlyContinue
+
+if ($notPresent)
+{
+    Write-Output "Creating " $config.resourceGroup "..."
+    New-AzResourceGroup `
+        -Name $config.resourceGroup `
+        -Location $config.location
+} else {
+    Write-Output "Resource group " $config.resourceGroup " already exists..."
+}
+
+# Create a subnet configuration
+Write-Output "Creating subnet configuration..."
+$subnetConfig = New-AzVirtualNetworkSubnetConfig `
+                    -Name $config.subnetName`
+                    -AddressPrefix 192.168.1.0/24
+
+# Create a virtual network
+Write-Output "Creating virtual network..."
+$vnet = New-AzVirtualNetwork `
+                -ResourceGroupName $config.resourceGroup `
+                -Location $config.location `
+                -Name $config.virtualNetworkName `
+                -AddressPrefix 192.168.0.0/16 `
+                -Subnet $subnetConfig
+
+# Create an inbound network security group rule for port 3389
+Write-Output "Creating inbound network security group for port 3389..."
+$nsgRuleRDP = New-AzNetworkSecurityRuleConfig `
+                    -Name $config.networkSecurityGroupRuleRDPName `
+                    -Protocol Tcp `
+                    -Direction Inbound `
+                    -Priority 1000 `
+                    -SourceAddressPrefix * `
+                    -SourcePortRange * `
+                    -DestinationAddressPrefix * `
+                    -DestinationPortRange 3389 `
+                    -Access Allow
+
+# Create a network security group
+Write-Output "Creating " $config.networkSecurityGroupName " network security group..."
+$nsg = New-AzNetworkSecurityGroup `
+            -ResourceGroupName $config.resourceGroup `
+            -Location $config.location `
+            -Name $config.networkSecurityGroupName `
+            -SecurityRules $nsgRuleRDP
+
+# Create virtual machine credentials
+Write-Output "Pulling virtual machine credentials..."
+$cred = Get-Credential -Message "Enter a username and password for the virtual machiness."
+
+foreach ($vm in $config.vms) {
+
+    # Create a public IP address and specify a DNS name
+    Write-Output "Creating public IP address and specifying DNS name..."
+    $pip = New-AzPublicIpAddress `
+                -ResourceGroupName $config.resourceGroup `
+                -Location $config.location `
+                -Name "mypublicdns$(Get-Random)" `
+                -AllocationMethod Static `
+                -IdleTimeoutInMinutes 4
+
+    # Create a virtual network card and associate with public IP address and NSG
+    Write-Output "Creating virtual network card and associating with public IP address and Network Security Group..."
+    $nic = New-AzNetworkInterface `
+                -Name ($config.virtualNetworkName + $vm.name) `
+                -ResourceGroupName $config.resourceGroup `
+                -Location $config.location `
+                -SubnetId $vnet.Subnets[0].Id `
+                -PublicIpAddressId $pip.Id `
+                -NetworkSecurityGroupId $nsg.Id
+
+    # Create a virtual machine configuration
+    Write-Output "Creating virtual machine configuration..."
+    $vmConfig = New-AzVMConfig -VMName $vm.name -VMSize $vm.size | `
+                Set-AzVMOperatingSystem -Windows -ComputerName $vm.name -Credential $cred | `
+                Set-AzVMSourceImage -PublisherName $vm.publisherName -Offer $vm.offer -Skus $vm.skus -Version latest | `
+                Add-AzVMNetworkInterface -Id $nic.Id
+
+    # Create a virtual machine
+    Write-Output "Creating virtual machine..."
+    New-AzVM `
+        -ResourceGroupName $config.resourceGroup `
+        -Location $config.location `
+        -VM $vmConfig `
+        -Verbose
+}
